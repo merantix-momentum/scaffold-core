cfea0ca37354574296e7c5057d6310fe7446c032
Martin Genzel <martingenzel@googlemail.com>
2025-02-28 14:11:25 +0100

    removed try catch block (not needed anymore)

--- src/scaffold/conf/__init__.py
+++ src/scaffold/conf/__init__.py
@@ -6,9 +6,6 @@ def register_all() -> None:
     from scaffold.conf.scaffold import artifact_manager  # noqa:F401
     from scaffold.conf.scaffold import entrypoint  # noqa:F401
 
-    try:
-        if importlib.util.find_spec("flytekit") is not None:
-            from scaffold.conf.scaffold import flyte_launcher  # noqa:F401
-    except ModuleNotFoundError:
-        # in case we didnt install flyte extra package
-        pass
+    if importlib.util.find_spec("flytekit") is not None:
+        # only import in case we installed flyte extra package
+        from scaffold.conf.scaffold import flyte_launcher  # noqa:F401
