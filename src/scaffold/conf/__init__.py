46f75a08fd2a68af46e51d4661add05de7494835
Martin Genzel <martingenzel@googlemail.com>
2025-02-28 11:29:09 +0100

    added an additional check for flyte to prevent user warning in scaffold config imports

--- src/scaffold/conf/__init__.py
+++ src/scaffold/conf/__init__.py
@@ -1,10 +1,14 @@
+import importlib.util
+
+
 def register_all() -> None:
     """All @structured_config classes register themselves when imported / defined"""
     from scaffold.conf.scaffold import artifact_manager  # noqa:F401
     from scaffold.conf.scaffold import entrypoint  # noqa:F401
 
     try:
-        from scaffold.conf.scaffold import flyte_launcher  # noqa:F401
+        if importlib.util.find_spec("flytekit") is not None:
+            from scaffold.conf.scaffold import flyte_launcher  # noqa:F401
     except ModuleNotFoundError:
         # in case we didnt install flyte extra package
         pass
