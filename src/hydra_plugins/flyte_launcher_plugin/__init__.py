9952a9cc652a9d6d52848068ca37ef4ab7c1d21a
Adrian Loy <adrian.loy@merantix-momentum.com>
2025-03-10 11:56:00 +0100

    Disable automatic discovery of flyte launcher plugin by hdyra, make the import and availability of flyte launcher plugin dependent on availability of flytekit

---
+++ src/hydra_plugins/flyte_launcher_plugin/__init__.py
@@ -0,0 +1,14 @@
+import importlib
+import logging
+
+if importlib.util.find_spec("flytekit") is None:
+    # if we cannot import flyte, importing flyte_launcher throws and error and we assume that
+    # in that case we do not want to have the plugin available at all.
+    # this ensure users who did not install flyte extra and dont need this plugin do not get
+    # a confusing error message (which could be ignored anyway)
+    logging.getLogger(__name__).warning("Could not import flytekit. Hydra flyte launcher plugin disabled")
+    __all__ = []
+else:
+    from . import _flyte_launcher as flyte_launcher
+
+    __all__ = [flyte_launcher]
