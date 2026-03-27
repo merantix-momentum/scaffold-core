5f5f33e9f54caf7519d77f09ef4752e637d20f25
Jonas Bernhard <jonasbernhard96@gmail.com>
2026-03-27 18:10:56 +0100

    Don't register LauncherConf to avoid conflict with user-added launcher config

--- src/scaffold/conf/scaffold/flyte_launcher.py
+++ src/scaffold/conf/scaffold/flyte_launcher.py
@@ -1,4 +1,5 @@
 from hydra_zen import builds, store
+from omegaconf import MISSING
 
 from hydra_plugins.flyte_launcher_plugin._flyte_launcher import (
     ExecutionEnvironmentEnum,
@@ -8,8 +9,6 @@ from hydra_plugins.flyte_launcher_plugin._flyte_launcher import (
     FlyteWorkflowConfig,
 )
 
-GROUP = "hydra/launcher"
-
 LauncherConf = builds(
     FlyteLauncher,
     execution_environment=ExecutionEnvironmentEnum.remote,
@@ -17,11 +16,10 @@ LauncherConf = builds(
     build_images=True,
     fast_serialization=False,
     run=True,
-    workflow=None,
+    workflow=MISSING,
     notifications=[],
 )
 
 store(FlyteDockerImageConfig, group="scaffold/flyte_launcher", name="FlyteDockerImageConfig")
 store(FlyteWorkflowConfig, group="scaffold/flyte_launcher", name="FlyteWorkflowConfig")
 store(FlyteNotificationConfig, group="scaffold/flyte_launcher", name="FlyteNotificationConfig")
-store(LauncherConf, group=GROUP, name="flyte")
