e38d32aa787b8c3a320891fcdde61d372866f831
Jonas Bernhard <jonasbernhard96@gmail.com>
2026-03-30 14:04:52 +0200

    Add entrypoint config to store

--- src/scaffold/conf/__init__.py
+++ src/scaffold/conf/__init__.py
@@ -9,6 +9,9 @@ def register_all() -> None:
     """Import all scaffold conf modules (triggering store() calls) then flush to hydra's ConfigStore."""
     from scaffold.conf.scaffold import artifact_manager  # noqa: F401
     from scaffold.conf.scaffold import ctx_manager  # noqa: F401
+    from scaffold.conf.scaffold import (  # noqa E501 NOTE: This module is deprecated and will be removed in a future release!; noqa: F401
+        entrypoint,
+    )
 
     if importlib.util.find_spec("flytekit") is not None:
         from scaffold.conf.scaffold import flyte_launcher  # noqa: F401
