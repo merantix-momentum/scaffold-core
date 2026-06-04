95455be24a17d1c5c6053b02e4f51df0e9b95764
Matias Miranda <matias.miranda@merantix-momentum.com>
2026-06-04 14:48:14 +0200

    add secret plugin for multiple providers aws, gcp, azure

---
+++ src/hydra_plugins/gcp_secrets_plugin/__init__.py
@@ -0,0 +1,5 @@
+"""GCP Secret Manager resolver for Hydra.
+
+Registers a custom OmegaConf resolver for GCP Secret Manager to enable secure,
+lazy-loaded secret injection into Hydra configurations.
+"""
